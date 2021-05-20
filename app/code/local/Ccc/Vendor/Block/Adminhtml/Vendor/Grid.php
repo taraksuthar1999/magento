3e15e222371eac47000610f8ecfa2ad400bd98d4
tarak suthar <tarak.suthar1999@gmail.com>
2021-05-20 13:45:57 +0530

    filter

--- app/code/local/Ccc/Vendor/Block/Adminhtml/Vendor/Grid.php
+++ app/code/local/Ccc/Vendor/Block/Adminhtml/Vendor/Grid.php
@@ -10,7 +10,7 @@ class Ccc_Vendor_Block_Adminhtml_Vendor_Grid extends Mage_Adminhtml_Block_Widget
         $this->setDefaultSort('entity_id');
         $this->setDefaultDir('DESC');
         $this->setSaveParametersInSession(true);
-        $this->setUseAjax(true);
+        $this->setUseAjax(false);
         $this->setVarNameFilter('vendor_filter');
     }
 
@@ -151,7 +151,7 @@ class Ccc_Vendor_Block_Adminhtml_Vendor_Grid extends Mage_Adminhtml_Block_Widget
 
     public function getGridUrl()
     {
-        return $this->getUrl('*/*/grid', array('_current' => true));
+        return $this->getUrl('*/*/index', array('_current' => true));
     }
 
     public function getRowUrl($row)
